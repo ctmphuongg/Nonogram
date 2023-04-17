d5998ffeb43d80c9086695278f998cba0a56760a
Linh Nguyen <lhn004@bucknell.edu>
2023-04-17 09:33:12 -0400

    gameResult

---
+++ src/main/java/org/team3/GameResult/GameResultController.java
@@ -0,0 +1,19 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Spring2023* Instructor: Prof. Brian King
+ ** Name: LINH NGUYEN
+ * Section: 9am
+ * Date: 17/04/2023
+ * Time: 09:15
+ *
+ * Project: csci205_final_project
+ * Package: org.team3.GameResult
+ * Class: GameResultController
+ *
+ * Description:
+ *
+ * *****************************************/
+package org.team3.GameResult;
+
+public class GameResultController {
+}
---
+++ src/main/java/org/team3/GameResult/GameResultView.java
@@ -0,0 +1,32 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Spring2023* Instructor: Prof. Brian King
+ ** Name: LINH NGUYEN
+ * Section: 9am
+ * Date: 17/04/2023
+ * Time: 09:14
+ *
+ * Project: csci205_final_project
+ * Package: org.team3.GameResult
+ * Class: GameResultView
+ *
+ * Description:
+ *
+ * *****************************************/
+package org.team3.GameResult;
+
+import javafx.scene.layout.VBox;
+
+public class GameResultView {
+
+    private VBox root;
+
+
+    public VBox getRoot() {
+        return root;
+    }
+
+
+
+
+}
