622337664dbed62976c621ee96bdf30ed99323d0
An Ngo <axn001@bucknell.edu>
2023-04-26 09:15:13 -0400

    An Ngo round button

---
+++ src/test/java/org/team3/model/PuzzleFactoryTest.java
@@ -0,0 +1,44 @@
+package org.team3.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PuzzleFactoryTest {
+
+    @Test
+    void getMatrix() {
+    }
+
+    @Test
+    void getRound() {
+    }
+
+    @Test
+    void getRowHint() {
+    }
+
+    @Test
+    void getColumnHint() {
+    }
+
+    @Test
+    void displayMatrix() {
+    }
+
+    @Test
+    void getColoredBox() {
+    }
+
+    @Test
+    void getPuzzleNumber() {
+    }
+
+    @Test
+    void setRound() {
+    }
+
+    @Test
+    void increaseRound() {
+    }
+}
---
+++ src/test/java/org/team3/model/RoundTest.java
@@ -0,0 +1,80 @@
+package org.team3.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RoundTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void setPlayingMode() {
+    }
+
+    @Test
+    void getPuzzleFactory() {
+    }
+
+    @Test
+    void getLivesValueArray() {
+    }
+
+    @Test
+    void getHints() {
+    }
+
+    @Test
+    void isIsWin() {
+    }
+
+    @Test
+    void isWinProperty() {
+    }
+
+    @Test
+    void getLives() {
+    }
+
+    @Test
+    void getPlayingMode() {
+    }
+
+    @Test
+    void checkValidGuess() {
+    }
+
+    @Test
+    void isRoundOver() {
+    }
+
+    @Test
+    void isRoundWinner() {
+    }
+
+    @Test
+    void guessEvaluator() {
+    }
+
+    @Test
+    void displayMatrix() {
+    }
+
+    @Test
+    void initNewRound() {
+    }
+
+    @Test
+    void isColored() {
+    }
+
+    @Test
+    void getHint() {
+    }
+
+    @Test
+    void getRoundState() {
+    }
+}
